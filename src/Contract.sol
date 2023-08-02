73a5e666bb1e2bc7e5e6ae44072d8b0bf7d19f36
Martin Halford <martin@benext.io>
2023-08-02 12:56:22 +1000

    Add Access Control for 'Depositor', 'Minter', 'Withdrawer', & 'Redeemer'
    Signed-off-by: Martin Halford <martin@benext.io>

--- src/Contract.sol
+++ src/Contract.sol
@@ -6,15 +6,22 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/IAccessControl.sol";
 
 /**
  * @title TokenizedVault
  * @dev This contract is a concrete implementation of ERC4626.
  */
-contract TokenizedVault is ERC4626, Ownable {
+contract TokenizedVault is ERC4626, Ownable, IAccessControl {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
+
+    // Define roles
+    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant WITHDRAWER_ROLE = keccak256("WITHDRAWER_ROLE");
+    bytes32 public constant REDEEMER_ROLE = keccak256("REDEEMER_ROLE");
 
     /**
      * @dev Sets the values for {name}, {symbol} and {_decimals}
@@ -24,9 +31,16 @@ contract TokenizedVault is ERC4626, Ownable {
         string memory name_,
         string memory symbol_
     ) ERC4626(asset_) ERC20(name_, symbol_) Ownable() {
+        // Set name, symbol and decimals for contract
         _name = name_;
         _symbol = symbol_;
         _decimals = ERC20(address(asset_)).decimals();
+
+        // Grant roles to msg.sender
+        _setupRole(DEPOSITOR_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(WITHDRAWER_ROLE, msg.sender);
+        _setupRole(REDEEMER_ROLE, msg.sender);
     }
 
     /**
@@ -71,4 +85,49 @@ contract TokenizedVault is ERC4626, Ownable {
     function decimals() public view virtual override(ERC4626) returns (uint8) {
         return _decimals;
     }
+
+    /**
+     * @dev Deposits `amount` of the underlying asset into the contract.
+     * Only accounts with the DEPOSITOR_ROLE can call this function.
+     */
+    function deposit(
+        uint256 amount,
+        address receiver
+    ) public override hasRole(DEPOSITOR_ROLE, msg.sender) {
+        super.deposit(amount, receiver);
+    }
+
+    /**
+     * @dev Mints `shares` of the underlying asset into the contract.
+     * Only accounts with the MINTER_ROLE can call this function.
+     */
+    function mint(
+        uint256 shares,
+        address receiver
+    ) public override hasRole(MINTER_ROLE, msg.sender) {
+        super.deposit(amount, receiver);
+    }
+
+    /**
+     * @dev Withdraws `amount` of the underlying asset from the contract.
+     * Only accounts with the WITHDRAWER_ROLE can call this function.
+     */
+    function withdraw(
+        uint256 amount,
+        address receiver
+    ) public override hasRole(WITHDRAWER_ROLE, msg.sender) {
+        super.withdraw(amount, receiver);
+    }
+
+    /**
+     * @dev Redeems `shares` of the underlying asset from the contract.
+     * Only accounts with the REDEEMER_ROLE can call this function.
+     */
+    function redeem(
+        uint256 shares,
+        address receiver
+    ) public override hasRole(REDEEMER_ROLE, msg.sender) {
+        super.redeem(shares, receiver);
+    }
+
 }
